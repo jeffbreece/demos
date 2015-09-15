63e7d6d5f3094fa21ab44c4b6e08e481d1f90161
Jeff Breece <jeff@jeffbreece.com>
2015-09-15 14:03:41 +0000

    Initial Check In
    BCS Assembly for demo purposes.

---
+++ DogFood/DogFood/Pto/PtoEntityService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace DogFood.Pto
+{
+    public partial class PtoEntityService
+    {
+
+        public static IEnumerable<TimeOff> ReadList()
+        {
+            PtoTableDataContext db = new PtoTableDataContext("");
+            var employeePtoData = from e in db.TimeOffs
+                              select new TimeOff
+                              {
+                                  EmployeeId = e.EmployeeId,
+                                  SickHours = e.SickHours,
+                                  PtoHours = e.PtoHours
+                              };
+            return employeePtoData;
+        }
+
+        public static TimeOff ReadItem(int employeeId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
